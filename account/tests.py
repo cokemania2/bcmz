a2ecd5e71b0221cc6b6b95aaccfd9b36390ecf8c
cokemania2 <coke_mania@naver.com>
2022-04-19 11:24:58 +0900

    회원가입 테스트코드 작성

--- account/tests.py
+++ account/tests.py
@@ -1,6 +1,7 @@
+import datetime
+
 from django.test import TestCase
 from django.utils import timezone
-from django.contrib.auth import get_user_model
 
 from rest_framework.test import APIClient
 
@@ -15,9 +16,17 @@ class BaseSetUpTest(TestCase):
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
-    def url_setup(self, url, method):
+    def setup_url(self, url, method):
         self.url = url
         self.method = method
+
+    def setup_token(self, number, accept):
+        finish_time = timezone.now()
+        if accept:
+            finish_time = finish_time + datetime.timedelta(minutes=5)
+        self.token = Token(phone_number=number,
+                           finish_time=finish_time,
+                           accepted=accept)
 
     def status_code_test(self, code, message, data=None):
         if self.method == 'POST':
@@ -26,20 +35,19 @@ class BaseSetUpTest(TestCase):
 
 
 class userViewTest(BaseSetUpTest):
-    def setup(self):
-        super().setup()
-        self.empty_data = {
-            'username': '',
-            'password': ''
-        }
+    def user_setup(self, email=None, nickname=None, password=None,
+                   username=None, phone_number=None):
         self.data = {
-            'username': 'test_username',
-            'password': 'test_password',
+            'email': email if email else 'test@test.com',
+            'nickname': nickname if nickname else 'testman',
+            'password': password if password else 'pw',
+            'username': username if username else 'testuser',
+            'phone_number': phone_number if phone_number else '01020647744'
         }
 
-    def test_auth(self):
+    def test_info_check(self):
         self.setup()
-        self.url_setup('/api/user/info_check/', 'POST')
+        self.setup_url('/api/user/info_check/', 'POST')
         self.status_code_test(200, "존재하는 username", {'username': '123'})
         self.status_code_test(200, "존재하는 nickname", {'nickname': 'testman'})
         self.status_code_test(200, "존재하는 조합", {'username': '123', 'nickname': 'testman'})
@@ -48,8 +56,32 @@ class userViewTest(BaseSetUpTest):
         self.status_code_test(404, "존재하는 않는 param", {'ff': '312'})
         # response = self.client.post('/api/mobile_auth/')
 
+    def test_sign_up(self):
+        self.setup_url('/api/user/sign_up/', 'POST')
+        self.user_setup()
+        self.status_code_test(404, "토큰 없는 회원가입", self.data)
+        self.setup_token('010206477444', False)
+        self.status_code_test(404, "토큰 미인증 회원가입", self.data)
+        self.setup_token('010206477444', True)
+        self.status_code_test(201, "정상적인 회원가입", self.data)
+        self.status_code_test(404, "중복 회원가입", self.data)
+
+        self.setup_token('01011112222', True)
+        self.user_setup(phone_number='01011112222')
+        self.status_code_test(404, "이메일 중복 회원가입", self.data)
+        self.user_setup(email='dummy@test.com')
+        self.status_code_test(404, "전화번호 중복 회원가입", self.data)
+
+        self.user_setup(email='test2@teset.com', nickname='coke', username='coke', phone_number='01012345678')
+        self.setup_token('01012345678', True)
+        self.status_code_test(201, "비밀번호 중복 회원가입", self.data)
+
+        self.setup_token('01033334444', True)
+        self.user_setup(email='test3@teset.com', username='coke3', phone_number='01033334444')
+        self.status_code_test(201, "닉네임 중복 회원가입", self.data)
+
     def test_token(self):
-        self.url_setup('/api/token/make_wait_token/', 'POST')
+        self.setup_url('/api/token/make_wait_token/', 'POST')
         self.status_code_test(
             201, "토큰 생성", {'phone_number': '01020647744', 'wait_time': '5'})
         self.status_code_test(
